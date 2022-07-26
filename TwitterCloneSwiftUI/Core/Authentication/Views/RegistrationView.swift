5e54c71a750f9400efa9f4f28e6d403f2f6b486b
Taewoo Kim <twk0128@naver.com>
2022-07-26 23:26:40 +0900

    -Sign Up UI -Firebase Setup / Integration

--- TwitterCloneSwiftUI/Core/Authentication/Views/RegistrationView.swift
+++ TwitterCloneSwiftUI/Core/Authentication/Views/RegistrationView.swift
@@ -8,28 +8,55 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State private var email = ""
+    @State private var username = ""
+    @State private var fullname = ""
+    @State private var password = ""
+    @Environment (\.dismiss) var dismiss
     var body: some View {
-        //parent container
-        VStack{
-            
+        VStack {
             //header view
-            VStack(alignment: .leading){
-                HStack {Spacer()}
+            AuthHeaderView(title1: "Get started.", title2: "Create your account")
+            
+            VStack(spacing: 40) {
+                CustomInputField(imageName: "envelope", placeholderText: "Email", text: $email)
                 
-                Text("Hello.")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
+                CustomInputField(imageName: "person", placeholderText: "Username", text: $email)
                 
-                Text("Welcome Back")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
+                CustomInputField(imageName: "person", placeholderText: "Full name", text: $password)
+                
+                CustomInputField(imageName: "lock", placeholderText: "Password", text: $password)
             }
-            .frame(height: 260)
-            .padding(.leading)
-            .background(Color(.systemBlue))
-            .foregroundColor(.white)
+            .padding(32)
+            
+            Button{
+                
+            } label: {
+                Text("Sign Up")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color(.systemBlue))
+                    .clipShape(Capsule())
+                    .padding()
+            }
+            .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 0)
             
             Spacer()
+            
+            Button{
+                dismiss()
+            } label: {
+                HStack{
+                    Text("Already have an account?")
+                        .font(.footnote)
+                    
+                    Text("Sign In")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding(.bottom, 32)
         }
         .ignoresSafeArea()
     }
